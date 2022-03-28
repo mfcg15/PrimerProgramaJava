6265a6d955412107717bef36fe239178465c67e4
mfcg15 <fcardenas1504@gmail.com>
2022-03-28 11:14:28 -0500

    Primer Programa en Java

---
+++ PrimerPrograma.java
@@ -0,0 +1,12 @@
+public class PrimerPrograma
+{
+    public static void main(String[] args) {
+        String nombre = "Martha Cardenas";
+        int edad = 23;
+        String ciduad = "Trujillo, Perú";
+
+        System.out.println("Mi nombre es "+nombre);
+        System.out.println("Tengo "+edad+" años de edad");
+        System.out.println("Mi ciudad natal es "+ciduad);
+    }
+}
